caecff81227c4033544970a5ab075204639a254f
SK3LLIX <77553577+SK3LLIX@users.noreply.github.com>
2021-09-10 19:16:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ function Hero(game, x, y) {
     // physics properties
     this.game.physics.enable(this);
     this.body.collideWorldBounds = true;
-    this.body.setOffset(0,-30);
 
     // animations
     this.animations.add('error', [0], 8, true);

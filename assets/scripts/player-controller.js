d38030e46570726c89ab1c283fa384baed1d0c8e
SK3LLIX <77553577+SK3LLIX@users.noreply.github.com>
2021-09-16 17:21:51 -0400

    Update player-controller.js

--- assets/scripts/player-controller.js
+++ assets/scripts/player-controller.js
@@ -13,6 +13,8 @@ function playercontroller ()
 
         player.anims.play('walk-left', true);
         
+        coins.anims.play('gold-spin', true);
+        
         player_direction = 'left';
         
         player_anim = 'walk';
